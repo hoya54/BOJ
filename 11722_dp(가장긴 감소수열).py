ef6873f3505c2654b3534615059c9aac082c635a
hoya54 <hoya9654@gmail.com>
2022-03-09 20:27:47 +0900

    220309

---
+++ 11722_dp(가장긴 감소수열).py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+lst = list(map(int, input().split()))
+
+result = [1]*N
+
+
+for i in range(N):
+    for j in range(i):
+        if lst[j] > lst[i]:
+            result[i] = max(result[j]+1, result[i])
+
+print(max(result))
+
