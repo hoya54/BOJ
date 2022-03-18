161c0f3ba07576f8617a0a8340fa190ed94bb8b2
hoya54 <hoya9654@gmail.com>
2022-03-18 20:33:07 +0900

    220318

---
+++ 1965_가장긴 증가수열.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+lst = list(map(int, input().split()))
+
+
+
+result = [1]*N
+
+for i in range(N):
+    for j in range(0, i):
+        if lst[j] < lst[i]: 
+            result[i] = max(result[i], result[j]+1)
+print(max(result))
