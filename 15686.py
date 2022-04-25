f00b84977d1825dcdfb37c8016fdf31ce3148d84
hoya54 <hoya9654@gmail.com>
2022-04-25 17:00:23 +0900

    220425

--- 15686.py
+++ 15686.py
@@ -1,6 +1,5 @@
 import sys
-import heapq
-
+from itertools import combinations
 
 input = sys.stdin.readline
 
@@ -8,68 +7,30 @@ N, M = map(int, input().split())
 
 ar = [[0 for _ in range(N)] for _ in range(N)]
 
-house = 0
-bbq = 0
-
-h = []
-b = []
+houses = []
+bbqs = []
 
 for i in range(N):
     st = list(map(int, input().split()))
     for j in range(N):
         ar[i][j] = st[j]
         if st[j] == 1:
-            house += 1
-            h.append((i, j))
+            houses.append((i, j))
         elif st[j] == 2:
-            bbq += 1
-            b.append((i, j))
+            bbqs.append((i, j))
 
-include = [0]*bbq
-lst = [[0 for _ in range(bbq)] for _ in range(house)]
-
-
-
-for i in range(house):
-    for j in range(bbq):
-        lst[i][j] = abs(h[i][0] - b[j][0]) + abs(h[i][1] - b[j][1])
-
-
-def func(t):
-    summ = 0
-    for i in range(house):
+min_distance = 10000
+for bbq_lst in combinations(bbqs, M):
+    sum_distance = 0
+    for house in houses:
         minn = 10000
+        for bbq in bbq_lst:
+            distance = abs(house[0] - bbq[0]) + abs(house[1] - bbq[1])
+            if distance < minn:
+                minn = distance
+        sum_distance += minn
+    if sum_distance < min_distance:
+        min_distance = sum_distance
 
-        for j in range(len(lst[0])):
-            if j == t:
-                continue
-            else:
-                if minn > lst[i][j]:
-                    minn = lst[i][j]
-        
-        summ += minn
-    
-    return summ
-
-print(lst)
-
-
-for t in range(bbq-M):
-    tmp = []
-    for i in range(len(lst[0])):
-        tmp.append(func(i))
-    
-    target = tmp.index(min(tmp))
-    #print(target)
-    #print(lst)
-    for i in range(house):
-        del lst[i][target]
-    #print(lst)
-
-summ=0
-for i in range(house):
-    summ += min(lst[i])
-
-print(summ)
-
+print(min_distance)
 
