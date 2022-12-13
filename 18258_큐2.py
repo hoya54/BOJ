a354445b2df80b7e0b7129527b6eccf0bbb66080
hoya54 <hoya9654@gmail.com>
2022-12-13 21:13:52 +0900

    22.12.13

---
+++ 18258_큐2.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+
+d = deque()
+
+for i in range(n):
+    lst = input().split()
+
+    print(type(lst))
+    
+    if(lst[0] == "push"):
+        d.append(int(lst[1]))
+    elif(lst[0] == "pop"):
+        if(len(d) == 0):
+            print(-1)
+        else:
+            print(d.popleft())
+    elif(lst[0] == "size"):
+        print(len(d))
+    elif(lst[0] == "empty"):
+        if(len(d) == 0):
+            print(1)
+        else:
+            print(0)
+    elif(lst[0] == "front"):
+        if(len(d) == 0):
+            print(-1)
+        else:
+            print(d[0])
+    elif(lst[0] == "back"):
+        if(len(d) == 0):
+            print(-1)
+        else:
+            print(d[-1])
+    
